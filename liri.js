e1262db7d295924a48c84caf0e50b7f9124e1bfa
Rodolfo Saez <rsaez@vmware.com>
2018-04-21 03:18:50 -0400

    created command conditionals

--- liri.js
+++ liri.js
@@ -1,3 +1,22 @@
+//makes the .env variables available
 require("dotenv").config();
+
+//sets up the api keys to variables
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+//Sets the user input to variables
+var command = process.argv[2];
+var input = process.argv[3];
+
+if(command === "my-tweets") {
+
+} else if(command === "spotify-this-song"){
+
+}else if(command === "movie-this"){
+
+}else if(command === "do-what-it-says") {
+
+} else {
+        console.log("Invalid command");
+}
